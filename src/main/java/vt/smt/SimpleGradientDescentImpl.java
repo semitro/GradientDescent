d4aa168e5100d6f23e33e6d669923b0facd1b085
semitro <semitro_8@mail.ru>
2018-09-22 23:21:10 +0300

    works with some epsilons

--- src/main/java/vt/smt/SimpleGradientDescentImpl.java
+++ src/main/java/vt/smt/SimpleGradientDescentImpl.java
@@ -6,14 +6,15 @@ import java.util.List;
 /**
  * Created by semitro on 22.09.18.
  */
-public class SimpleGradientDescentImpl implements  GradientDescent {
+public class SimpleGradientDescentImpl implements GradientDescent {
 
 
-    public SimpleGradientDescentImpl(double[][] dataset){
+    public SimpleGradientDescentImpl(double[][] dataset) {
         this.dataset = dataset;
     }
 
     private double[][] dataset;
+
     /*
      *
      * On the one hand, I could not used theta0
@@ -26,22 +27,29 @@ public class SimpleGradientDescentImpl implements  GradientDescent {
         double augmentation = epsilon + 1; // average difference between new thetaj and old thetaj
         double theta0 = 0.; //  the bias of the prediction model
         double nextTheta0 = 0.0;
-        final double thetas[]     = new double[dataset[0].length];
+        final double thetas[] = new double[dataset[0].length - 1];
 
         final double nextThetas[] = new double[thetas.length];
 
-        while( augmentation > epsilon){
-            for(int t = 0; t < thetas.length; t++){
-                double accum = 0.;
-                for(double[] sample : dataset){
+        while (augmentation > epsilon) {
+
+            double accum = 0.;
+            for (double[] sample : dataset) {
+                double prediction = approximateFunction(thetas, sample, theta0);
+                accum += sample[sample.length - 1] - prediction;
+            }
+
+            nextTheta0 += accum* step / dataset.length;
+            System.out.println(nextTheta0);
+            for (int t = 0; t < thetas.length; t++) {
+                for (double[] sample : dataset) {
                     double prediction = approximateFunction(thetas, sample, theta0);
-                    accum      += sample[sample.length -1 ] * prediction * sample[t];
-                    nextTheta0 += sample[sample.length -1 ] * prediction;
+                    accum += (sample[sample.length - 1] - prediction) * sample[t]; // sample[last] = y real
                 }
-                nextThetas[t] += step*accum/dataset.length;
+                nextThetas[t] += step * accum / dataset.length;
                 augmentation += nextThetas[t] - thetas[t];
             }
-            nextTheta0 *= step/dataset.length;
+
             augmentation += nextTheta0 - theta0;
             augmentation /= thetas.length + 1; // make it average. +1 because of theta0
 
@@ -49,17 +57,17 @@ public class SimpleGradientDescentImpl implements  GradientDescent {
             theta0 = nextTheta0;
         }
 
-        List<Double> res = new ArrayList<>(thetas.length+1);
+        List<Double> res = new ArrayList<>(thetas.length + 1);
         res.add(theta0);
         for (double theta : thetas) res.add(theta);
 
         return res;
     }
 
-    private double approximateFunction(double thetas[], double x[], double theta0){
-        for (int i = 0; i < thetas.length ; i++)
-           theta0 += thetas[i]*x[i];
+    private double approximateFunction(double thetas[], double x[], double theta0) {
+        for (int i = 0; i < thetas.length; i++)
+            theta0 += thetas[i] * x[i];
 
-        return  theta0;
+        return theta0;
     }
 }
--- src/test/java/vt/smt/GradientDescentSimpleTest.java
+++ src/test/java/vt/smt/GradientDescentSimpleTest.java
@@ -9,22 +9,28 @@ public class GradientDescentSimpleTest {
 
     @Test
     public void testAngle45(){
-        double precisely45line[][] = { {1., 1.}, {2.,2.}, {3.,3.}};
+        double precisely45line[][] = { {1., 1.}, {2.,2.}, {3.,3.}, {300.0, 300.0}};
         SimpleGradientDescentImpl descent = new SimpleGradientDescentImpl(precisely45line);
-        System.out.println(descent.minimizeErrorFunction(0.5, 0.1));
+        System.out.println(descent.minimizeErrorFunction(0.00005, 0.000001));
     }
 
     @Test
     public void testWithTheta0(){
-      double prec45Line1[][] = { {1., 2.}, {2., 3.}, {3.,4.}};
+      double prec45Line1[][] = { {100., 121.}, {2., 22.}, {3.,25.}, {0.0, 20.}};
+        SimpleGradientDescentImpl descent = new SimpleGradientDescentImpl(prec45Line1);
+        System.out.println(descent.minimizeErrorFunction(0.0000005, 0.0001));
 
     }
-
+    @Test
     public void testNearly30(){
       double nearly30[][] = { {1.9, 0.9}, {4.1, 8.12}, {1.7, 0.9}, {100.0, 221.12}};
+        SimpleGradientDescentImpl descent = new SimpleGradientDescentImpl(nearly30);
+        System.out.println(descent.minimizeErrorFunction(0.00005, 0.00001));
     }
-
+    @Test
     public void multiDimens(){
         double yIs2x5x7xminus2x[][] = { {1.14, 0.85, 1.24, 1.14, 1.15, }, {} };
+        SimpleGradientDescentImpl descent = new SimpleGradientDescentImpl(yIs2x5x7xminus2x);
+        System.out.println(descent.minimizeErrorFunction(0.00005, 0.01));
     }
 }
